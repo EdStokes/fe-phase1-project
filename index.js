3864ac7244ee5013b0081287bdfcea1f3e0ab10b
Ed Stokes <edstokes96@gmail.com>
2023-06-13 15:18:20 -0500

    modified function renderCategoryOptions to ensure categories selected have more then 10 questions

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const trivaCategory = 'https://jservice.io/api/categories?count=15';
+//const trivaQuestions = `https://jservice.io/api/categories?id=${selectedCategory}`
 
 //Elements
 const categorySelect = document.querySelector("#category");
 
 //Function calls
 getCategories()
+
+//event listener
+categorySelect.addEventListener("change", getQuestionsByCategory)
+
+
 function getCategories() {
     fetch(trivaCategory)
     .then(res => res.json())
@@ -13,11 +19,25 @@ function getCategories() {
 
 function renderCategoryOptions(categories){
     categories.forEach(category => {
+        if (category.clues_count >= 10) {
         const option = document.createElement("option");
-        option.value = category.title;
+        option.value = category.id;
         option.textContent = category.title;
         categorySelect.append(option);
-    })
+        }
+})
 
 }
 
+function getQuestionsByCategory(e) {
+    const selectedCategory = e.target.value;
+
+    
+    fetch(`https://jservice.io/api/clues?category=${selectedCategory}`)
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+
+function randomNumberCategory() {
+
+}
