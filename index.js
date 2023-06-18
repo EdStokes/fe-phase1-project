c81b7c13a019cf232e15105f6a2464d1e2c2f4af
Ed Stokes <edstokes96@gmail.com>
2023-06-18 18:37:58 -0500

    added cards for questions to display on

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const categoryStart = getRandomNumber(1, 85);
 const trivaCategory = `https://jservice.io/api/categories?count=15`;
-const questionArray = [];
 //const trivaQuestions = `https://jservice.io/api/categories?id=${selectedCategory}`
 
 //Elements
@@ -8,12 +7,13 @@ const categorySelect = document.querySelector("#category");
 const categoryElement = document.querySelector(".category-container");
 const questionContainer = document.querySelector(".question-container");
 
+
 //Function calls
 getCategories()
 
 
 //event listener
-// categorySelect.addEventListener("change", getQuestionsByCategory)
+
 
 
 function getRandomNumber(min, max) {
@@ -31,51 +31,33 @@ function renderCategoryOptions(categories) {
         if (category.clues_count >= 10) {
             const categoryCard = document.createElement('div');
             categoryCard.classList.add('cardCategory');
-            categoryCard.addEventListener("click", function() {
+            categoryCard.addEventListener("click", function () {
                 getQuestionsByCategory(category.id)
             })
             categoryCard.innerHTML = category.title.toUpperCase();
-            questionContainer.append(categoryCard)
+            categoryElement.append(categoryCard)
         }
     })
-
 }
-
-
 
 function getQuestionsByCategory(categoryId) {
     const selectedCategory = categoryId;
-    console.log(selectedCategory)
-
     fetch(`https://jservice.io/api/clues?category=${selectedCategory}`)
         .then(res => res.json())
-        .then(questions => createQuestionArray(questions));
+        .then(questions => renderQuestions(questions));
 }
 
-function createQuestionArray(questions) {
+function renderQuestions(questions) {
+    const questionArray = [];
     questions.forEach(question => {
-        questionArray.push(question)
+        questionArray.push(question);
     })
-    console.log(questionArray)
-    console.log(questionArray[0].question)
-    console.log(questionArray[0].answer)
-
-    return questionArray;
-
-}
-
-function renderQuestionCard(question) {
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
-
-    const cardQuestion = document.createElement("h2");
-    cardQuestion.textContent = questionArray[1].question;
-
-    cardDiv.append(cardQuestion);
-    questionContainer.append(cardDiv)
-
-
+    const questionCard = document.createElement('div');
+    questionCard.classList.add('questionCard');
+    questionCard.innerHTML = questionArray[1].question;
+    questionContainer.append(questionCard)
 
 }
-console.log("is this working")
-//console.log(questionArray[1].question);
+
+
+
