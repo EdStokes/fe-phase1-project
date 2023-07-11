90eacd246af1c0515f349b8d115ca6e6947d05c8
Ed Stokes <edstokes96@gmail.com>
2023-07-11 11:49:43 -0500

    updated comments in the index.html file

--- index.js
+++ index.js
@@ -18,12 +18,14 @@ const title = document.createElement("h1");
 title.textContent = "Triva";
 titleElement.append(title);
 
+// get the categories form the API
 function getCategories() {
     fetch(trivaCategory)
         .then(res => res.json())
         .then(categories => renderCategoryOptions(categories));
 }
 
+// render the categories to the top of the screen
 function renderCategoryOptions(categories) {
     categories.forEach(category => {
         if (category.clues_count >= 10) {
@@ -38,6 +40,7 @@ function renderCategoryOptions(categories) {
     })
 }
 
+// get the questions from the API based on the category selected
 function getQuestionsByCategory(categoryId) {
     const selectedCategory = categoryId;
     answerContainer.replaceChildren();
@@ -48,6 +51,7 @@ function getQuestionsByCategory(categoryId) {
         .then(questions => renderQuestions(questions));
 }
 
+// place the question into an array
 function renderQuestions(questions) {
     const questionArray = [];
     const nextButton = document.createElement('button');
@@ -67,6 +71,7 @@ function renderQuestions(questions) {
     })
 }
 
+//display the question on the screen
 function questionCard(questions, index) {
     const questionCard = document.createElement('div');
     const answer = document.createElement('button');
@@ -88,6 +93,7 @@ function questionCard(questions, index) {
     questionInfoElement.replaceChild(currentQuestionNumber, replaceQuestion)
 }
 
+//create an infor bar to display category and question infor
 function questionInfo(question, index) {
     const categoryInfo = document.createElement('h3');
     const category = question[0].category.title.toUpperCase();
@@ -101,6 +107,7 @@ function questionInfo(question, index) {
     questionInfoElement.append(categoryInfo)
 }
 
+//show the answer to the question
 function answerCard(questions, index) {
     const answerCard = document.createElement('p');
     answerCard.classList.add('answerCard')
